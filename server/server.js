5e807a2c00649eef65fe8bc9ac6bf2147dfe83ca
AKALIA Slimane <slimaneakalia@gmailcom>
2017-07-19 12:36:33 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find({}).then((todos) => {
+		res.send(todos);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+}, (err) => res.status(400).send(err));
+
 app.listen(PORT, () => {
 	console.log("Server started on port : ", PORT);
 });
