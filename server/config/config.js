3e19c5afbef63d5b5bd585760dcaa0bbfe7295ae
AKALIA Slimane <slimaneakalia@gmailcom>
2017-07-20 19:20:47 +0000

    Setup separate development and test envs 2

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ const env = process.env.NODE_ENV || 'development';
 
 if (env == 'development')
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp_m';
-else (env == 'test')
+else if (env == 'test')
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp_m_test';
