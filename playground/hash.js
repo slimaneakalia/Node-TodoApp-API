2ea2d60d12041b92946df77502c59280eb51f3e0
AKALIA Slimane <slimaneakalia@gmailcom>
2017-07-22 18:35:48 +0000

    Hash user password before saving

--- playground/hash.js
+++ playground/hash.js
@@ -1,18 +1,32 @@
-const { SHA256 } = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id : 5
-}
-var token = jwt.sign(data, 'HelloWorld!');
-console.log(token);
+var pass = 'HelloWorld!';
+bcrypt.genSalt (10, (err, salt) => {
+	bcrypt.hash(pass, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
 
-try {
-	var decoded = jwt.verify(token, 'HelloWorld');
-	console.log('decoded : ', decoded);
-}catch(e){
-	console.log(e.message);
-}
+var hash = '$2a$10$GEl4.Jv8NR.Vndgs95HgXuVtZFjOVQQYnbcNhxNtVgDU7bt07e9w6';
+bcrypt.compare(pass, hash, (err, res) => {
+	console.log("Res : ", res);
+});
+
+// const { SHA256 } = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+
+// var data = {
+// 	id : 5
+// }
+// var token = jwt.sign(data, 'HelloWorld!');
+// console.log(token);
+
+// try {
+// 	var decoded = jwt.verify(token, 'HelloWorld');
+// 	console.log('decoded : ', decoded);
+// }catch(e){
+// 	console.log(e.message);
+// }
 
 /*
 var message = 'Hello world !!';
